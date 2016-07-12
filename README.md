fc27c50d57a1a7ac3d4134e793c453e41b43c9db
Tarun Mathew <crushd@gmail.com>
2016-07-12 20:27:08 +0530

    roman to arabic conversion sans error checking

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ One half of the solution done. To convert from arabic to roman numerals.
 As of now input is not given at runtime. Input is hardcoded into the 
 variable num2 for each instance of conversion. I'll be changing it to 
 accept the input at runtime.
+
+Reverse translation : Roman to Arabic done sans error checking. Here 
+again, the input is hard coded into the program using the variable num4.
--- roman-arabic.js
+++ roman-arabic.js
@@ -10,16 +10,18 @@
     var len;
     var digit;
 
-    // var romanMap = {
-    //   "I":"1",
-    //   "V":"5",
-    //   "X":"10",
-    //   "L":"50",
-    //   "C":"100",
-    //   "D":"500",
-    //   "M":"1000"
-    // };
+    var arabicToRomanMap = {
+      1:"I",
+      5:"V",
+      10:"X",
+      50:"L",
+      100:"C",
+      500:"D",
+      1000:"M"
+    };
 
+    //console.log(arabicToRomanMap[5]);
+    
     //split the number into 100s and 10s and 1s
     num=num2;
 
@@ -68,11 +70,10 @@
         var t=str[i];
         if(str[i]===1||str[i]===2||str[i]===3)
         { 
-          // console.log(str[i]);
-          // var t=str[i];
-          for (var k = 0; k < t; k++) {
-            console.log('C');
-          };
+          // for (var k = 0; k < t; k++) {
+          //   console.log('C');
+          // };
+          console.log('C'.repeat(t));
         }
         else if(str[i]===4)
         {
@@ -156,6 +157,61 @@
       }
       len--;
       i++;
-    
 
+     
+     // converting roman numerals to arabic
+
+     // var num4 = ['C','C','C','L','X','V']; //365
+      // var num4 = ['M','C','C','C','X','X','X','I','X']; //1339
+      var num4 = ['D','C','X','L','I','I','I']; //643
+     i=0;
+     len=num4.length;
+     var a=[]; 
+     var sum=0,flag=0;
+
+     for (var i = 0; i < len; i++) {
+       if (num4[i]==='M')
+        a[i]=1000;
+        else if (num4[i]==='D')
+          a[i]=500;
+        else if (num4[i]==='C')
+          a[i]=100;
+        else if (num4[i]==='L')
+         a[i]=50;
+        else if (num4[i]==='X')
+          a[i]=10;
+        else if (num4[i]==='V')
+          a[i]=5
+        else if (num4[i]==='I')
+          a[i]=1;
+     };
+
+     for (var i = 0; i < len; i++) {
+       // console.log(i);
+       if(flag===1)
+       {
+        flag=0;
+        continue;
+       }
+
+       if (a[i]<a[i+1])
+       {
+          sum = sum + (a[i+1]-a[i]);
+          // console.log(i);
+          flag=1;
+       }
+       else if(a[i]>=a[i+1])
+       {
+          sum = sum + a[i];
+          // console.log(i);
+       }
+       else
+        {
+          sum=sum+a[i];
+        }
+     };
+    
+     console.log("Equavalent arabic : "+sum);
+
+    
 
